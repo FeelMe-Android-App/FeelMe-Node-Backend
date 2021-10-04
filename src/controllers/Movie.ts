a1111848735b839c6b3a346305ab4dda2a48fdfb
Paulo Wuéliton <paulo.wueliton@hotmail.com>
2021-10-03 21:57:44 -0300

    fix: movie result

--- src/controllers/Movie.ts
+++ src/controllers/Movie.ts
@@ -1,7 +1,6 @@
 import { Request, Response } from "express";
 import { User } from "../models/User";
 import { Movie } from "../models/Movie";
-import admin from "firebase-admin";
 import { IMovie } from "../types/Movie";
 
 export const getUnwatchedMovieList = async (req: Request, res: Response) => {
@@ -19,7 +18,7 @@ export const getUnwatchedMovieList = async (req: Request, res: Response) => {
       .limit(20);
     if (!movieList || movieList.length === 0)
       return res.status(422).json({ error: "No more itens to show" });
-    return res.status(200).json(movieList.length);
+    return res.status(200).json(movieList);
   } catch (err) {
     res.status(404).json({ error: "Error, please try again" });
   }
