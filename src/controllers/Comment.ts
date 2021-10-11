fc998dcbf820f51c355b82b4ae23765654969f7f
Paulo <paulo.wueliton@hotmail.com>
2021-10-11 14:04:21 -0300

    fix: movie comments result

--- src/controllers/Comment.ts
+++ src/controllers/Comment.ts
@@ -59,7 +59,7 @@ export const getMovieComments = async (req: Request, res: Response) => {
 
     if (!comments || comments.length === 0)
       return res.status(422).json({ error: "No more itens to show" });
-    return res.status(200).json(comments);
+    return res.status(200).json({ comments: comments });
   } catch (err) {
     res.status(404).json({ error: "Error, please try again" });
   }
